9775e42265a6ed7eb33ba690d8e4b4e9269e2da3
Fantomas42 <fantomas42@gmail.com>
2013-06-17 16:11:28 +0200

    Use Class Based Views for compatibility with django 1.5

--- categories/urls.py
+++ categories/urls.py
@@ -1,14 +1,16 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import url
+from django.conf.urls import patterns
+from django.views.genetric import ListView
+
 from .models import Category
 
-categorytree_dict = {
-    'queryset': Category.objects.filter(level=0)
-}
+class CategoryTreeList(ListView):
+    queryset = Category.objects.filter(level=0)
 
-urlpatterns = patterns('django.views.generic.list_detail',
-    url(
-        r'^$', 'object_list', categorytree_dict, name='categories_tree_list'
-    ),
+
+urlpatterns = patterns('',
+    url(r'^$', CategoryTreeList.as_view(),
+        name='categories_tree_list'),
 )
 
 urlpatterns += patterns('categories.views',
